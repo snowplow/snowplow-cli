ea922623665bcaa3f91e788f2c54aeb1fb40f079
Gleb Lobov <gleb.lobov@snowplowanalytics.com>
2024-08-21 12:23:17 +0200

    [DPS-145] Add test

---
+++ cmd/data-structure_test.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestDataStructuJsonParseSuccess(t *testing.T) {
+	jsonString := string(`{
+	  "meta": {
+		"hidden": true,
+		"schemaType": "entity",
+		"customData": {
+		  "additionalProp1": "string",
+		  "additionalProp2": "string",
+		  "additionalProp3": "string"
+		}
+	  },
+	  "data": {
+		"self": {
+		  "vendor": "string",
+		  "name": "string",
+		  "format": "string",
+		  "version": {
+			"model": 1073741824,
+			"revision": 1073741824,
+			"addition": 1073741824
+		  }
+		},
+		"schema": "string"
+	  }
+	}`)
+	expected := DataStructure{
+		Meta: DataStructureMeta{Hidden: true, SchemaType: "entity", CustomData: map[string]string{
+			"additionalProp1": "string",
+			"additionalProp2": "string",
+			"additionalProp3": "string",
+		},
+		},
+		Data: DataStrucutreData{
+			Self: DataStructureSelf{
+				Vendor: "string",
+				Name:   "string",
+				Format: "string",
+				Version: DataStructureVersion{
+					Model:    1073741824,
+					Revision: 1073741824,
+					Addition: 1073741824,
+				},
+			},
+			Schema: "string"},
+	}
+	res := DataStructure{}
+	err := json.Unmarshal([]byte(jsonString), &res)
+	if !reflect.DeepEqual(expected, res) || err != nil {
+		t.Fatalf("Cant' parse json %s\n parsed %#v\n expected %#v", err, res, expected)
+	}
+
+}
