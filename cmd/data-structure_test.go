92778714225188447f271865a70d1896a0edd531
gleb-lobov <126697986+gleb-lobov@users.noreply.github.com>
2024-08-21 15:10:37 +0200

    [DPS-151] Add CI (#5)

--- cmd/data-structure_test.go
+++ cmd/data-structure_test.go
@@ -5,35 +5,36 @@ package cmd
 
 import (
 	"encoding/json"
+	"gopkg.in/yaml.v3"
 	"reflect"
 	"testing"
 )
 
-func TestDataStructuJsonParseSuccess(t *testing.T) {
+func TestDataStructureJsonParseSuccess(t *testing.T) {
 	jsonString := string(`{
-	  "meta": {
-		"hidden": true,
-		"schemaType": "entity",
-		"customData": {
-		  "additionalProp1": "string",
-		  "additionalProp2": "string",
-		  "additionalProp3": "string"
-		}
-	  },
-	  "data": {
-		"self": {
-		  "vendor": "string",
-		  "name": "string",
-		  "format": "string",
-		  "version": {
-			"model": 1073741824,
-			"revision": 1073741824,
-			"addition": 1073741824
-		  }
-		},
-		"schema": "string"
-	  }
-	}`)
+      "meta": {
+        "hidden": true,
+        "schemaType": "entity",
+        "customData": {
+          "additionalProp1": "string",
+          "additionalProp2": "string",
+          "additionalProp3": "string"
+        }
+      },
+      "data": {
+        "self": {
+          "vendor": "string",
+          "name": "string",
+          "format": "string",
+          "version": {
+            "model": 1073741824,
+            "revision": 1073741824,
+            "addition": 1073741824
+          }
+        },
+        "schema": "string"
+      }
+    }`)
 	expected := DataStructure{
 		Meta: DataStructureMeta{Hidden: true, SchemaType: "entity", CustomData: map[string]string{
 			"additionalProp1": "string",
@@ -62,34 +63,80 @@ func TestDataStructuJsonParseSuccess(t *testing.T) {
 
 }
 
-func TestDataStructuJsonParseFailureWrongFormat(t *testing.T) {
+func TestDataStructureJsonParseFailureWrongFormat(t *testing.T) {
 	jsonString := string(`{
-	  "meta": {
-		"hidden": true,
-		"schemaType": "entity",
-		"customData": {
-		  "additionalProp1": "string",
-		  "additionalProp2": "string",
-		  "additionalProp3": "string"
-		}
-	  },
-	  "data": {
-		"self": {
-		  "vendor": "string",
-		  "name": "string",
-		  "format": "string",
-		  "version": {
-			"model": 1073741824,
-			"revision": 1073741824,
-			"addition": 1073741824
-		  }
-		},
-	  }
-	}`)
+      "meta": {
+        "hidden": true,
+        "schemaType": "entity",
+        "customData": {
+          "additionalProp1": "string",
+          "additionalProp2": "string",
+          "additionalProp3": "string"
+        }
+      },
+      "data": {
+        "self": {
+          "vendor": "string",
+          "name": "string",
+          "format": "string",
+          "version": {
+            "model": 1073741824,
+            "revision": 1073741824,
+            "addition": 1073741824
+          }
+        },
+      }
+    }`)
 	res := DataStructure{}
 	err := json.Unmarshal([]byte(jsonString), &res)
 	if err == nil {
 		t.Fatal("Parsed data structure without schema")
+	}
+
+}
+
+func TestDataStructureYamlParseSuccess(t *testing.T) {
+	yamlString := string(`meta:
+  hidden: true
+  schemaType: entity
+  customData:
+    additionalProp1: string
+    additionalProp2: string
+    additionalProp3: string
+data:
+  self:
+    vendor: string
+    name: string
+    format: string
+    version:
+      model: 1073741824
+      revision: 1073741824
+      addition: 1073741824
+  schema: string`)
+	expected := DataStructure{
+		Meta: DataStructureMeta{Hidden: true, SchemaType: "entity", CustomData: map[string]string{
+			"additionalProp1": "string",
+			"additionalProp2": "string",
+			"additionalProp3": "string",
+		},
+		},
+		Data: DataStrucutreData{
+			Self: DataStructureSelf{
+				Vendor: "string",
+				Name:   "string",
+				Format: "string",
+				Version: DataStructureVersion{
+					Model:    1073741824,
+					Revision: 1073741824,
+					Addition: 1073741824,
+				},
+			},
+			Schema: "string"},
+	}
+	res := DataStructure{}
+	err := yaml.Unmarshal([]byte(yamlString), &res)
+	if !reflect.DeepEqual(expected, res) || err != nil {
+		t.Fatalf("Cant' parse yaml %s\n parsed %#v\n expected %#v", err, res, expected)
 	}
 
 }
