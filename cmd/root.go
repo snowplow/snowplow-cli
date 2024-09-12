4a8e7663373351113e0338ef432257fdbc3da4ef
Gleb Lobov <gleb.lobov@snowplowanalytics.com>
2024-09-12 15:56:40 +0200

    Rename to snowplow-cli

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Use:   "dps-cli",
+	Use:   "snowplow-cli",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
