b58af232a4d20aa17305883b222b72faa71e9f95
Gleb Lobov <gleb.lobov@snowplowanalytics.com>
2024-09-25 09:36:59 +0200

    [DPS-287] Description

--- cmd/root.go
+++ cmd/root.go
@@ -19,13 +19,10 @@ import (
 
 var RootCmd = &cobra.Command{
 	Use:   "snowplow-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Command line tool to work with Snowplow",
+	Long: `
+	data-structures - manage data-structures as yaml/json files: download, edit, publish, author
+	`,
 }
 
 func Execute() {
