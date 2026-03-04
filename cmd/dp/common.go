ae73dab92a7fd3f264ec4f7fe31f25c9eb3f5950
Gleb Lobov <gleb.lobov@snowplowanalytics.com>
2026-03-04 17:56:10 +0100

    [DPS-1576] Abstract away similarities

---
+++ cmd/dp/common.go
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2013-present Snowplow Analytics Ltd.
+ * All rights reserved.
+ * This software is made available by Snowplow Analytics, Ltd.,
+ * under the terms of the Snowplow Limited Use License Agreement, Version 1.0
+ * located at https://docs.snowplow.io/limited-use-license-1.0
+ * BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY PORTION
+ * OF THE SOFTWARE, YOU AGREE TO THE TERMS OF SUCH LICENSE AGREEMENT.
+ */
+
+package dp
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/snowplow/snowplow-cli/internal/console"
+	snplog "github.com/snowplow/snowplow-cli/internal/logging"
+	"github.com/snowplow/snowplow-cli/internal/release"
+	"github.com/snowplow/snowplow-cli/internal/util"
+	"github.com/snowplow/snowplow-cli/internal/validation"
+	"github.com/spf13/cobra"
+)
+
+type dpAction func(context.Context, *console.ApiClient, *release.DataProductChangeSet, bool) error
+
+func runDpWorkflow(cmd *cobra.Command, args []string, action dpAction) {
+	apiKeyId, _ := cmd.Flags().GetString("api-key-id")
+	apiKeySecret, _ := cmd.Flags().GetString("api-key")
+	host, _ := cmd.Flags().GetString("host")
+	org, _ := cmd.Flags().GetString("org-id")
+	dryRun, _ := cmd.Flags().GetBool("dry-run")
+	ghOut, _ := cmd.Flags().GetBool("gh-annotate")
+	managedFrom, _ := cmd.Flags().GetString("managed-from")
+	concurrentReq, _ := cmd.Flags().GetInt("concurrency")
+
+	if concurrentReq > 10 {
+		concurrentReq = 10
+		slog.Debug("validation", "msg", "concurrency set to > 10, limited to 10")
+	}
+
+	if concurrentReq < 1 {
+		concurrentReq = 1
+		slog.Debug("validation", "msg", "concurrency set to < 1, increased to 1")
+	}
+
+	searchPaths := []string{}
+
+	if len(args) == 0 {
+		searchPaths = append(searchPaths, util.DataProductsFolder)
+		slog.Debug("validation", "msg", fmt.Sprintf("no path provided, using default (./%s)", util.DataProductsFolder))
+	}
+
+	searchPaths = append(searchPaths, args...)
+
+	files, err := util.MaybeResourcesfromPaths(searchPaths)
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+
+	basePath, err := os.Getwd()
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+
+	cnx := context.Background()
+
+	c, err := console.NewApiClient(cnx, host, apiKeyId, apiKeySecret, org)
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+
+	changes, err := release.FindChanges(cnx, c, files)
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+
+	release.LockChanged(changes, managedFrom)
+
+	err = validation.Validate(cnx, c, files, searchPaths, basePath, ghOut, false, changes.IdToFileName, concurrentReq)
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+
+	err = action(cnx, c, changes, dryRun)
+	if err != nil {
+		snplog.LogFatal(err)
+	}
+}
+
+func addCommonDpFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().Bool("gh-annotate", false, "Output suitable for github workflow annotation (ignores -s)")
+	cmd.PersistentFlags().BoolP("dry-run", "d", false, "Only print planned changes without performing them")
+	cmd.PersistentFlags().IntP("concurrency", "c", 3, "The number of validation requests to perform at once (maximum 10)")
+}
--- cmd/dp/sync.go
+++ cmd/dp/sync.go
@@ -12,15 +12,9 @@ package dp
 
 import (
 	"context"
-	"fmt"
-	"log/slog"
-	"os"
 
 	"github.com/snowplow/snowplow-cli/internal/console"
-	snplog "github.com/snowplow/snowplow-cli/internal/logging"
 	"github.com/snowplow/snowplow-cli/internal/release"
-	"github.com/snowplow/snowplow-cli/internal/util"
-	"github.com/snowplow/snowplow-cli/internal/validation"
 	"github.com/spf13/cobra"
 )
 
@@ -38,74 +32,13 @@ If no directory is provided then defaults to 'data-products' in the current dire
 	Example: `  $ snowplow-cli dp sync
   $ snowplow-cli dp sync ./my-data-products`,
 	Run: func(cmd *cobra.Command, args []string) {
-		apiKeyId, _ := cmd.Flags().GetString("api-key-id")
-		apiKeySecret, _ := cmd.Flags().GetString("api-key")
-		host, _ := cmd.Flags().GetString("host")
-		org, _ := cmd.Flags().GetString("org-id")
-		dryRun, _ := cmd.Flags().GetBool("dry-run")
-		ghOut, _ := cmd.Flags().GetBool("gh-annotate")
-		managedFrom, _ := cmd.Flags().GetString("managed-from")
-		concurrentReq, _ := cmd.Flags().GetInt("concurrency")
-
-		if concurrentReq > 10 {
-			concurrentReq = 10
-			slog.Debug("validation", "msg", "concurrency set to > 10, limited to 10")
-		}
-
-		if concurrentReq < 1 {
-			concurrentReq = 1
-			slog.Debug("validation", "msg", "concurrency set to < 1, increased to 1")
-		}
-
-		searchPaths := []string{}
-
-		if len(args) == 0 {
-			searchPaths = append(searchPaths, util.DataProductsFolder)
-			slog.Debug("validation", "msg", fmt.Sprintf("no path provided, using default (./%s)", util.DataProductsFolder))
-		}
-
-		searchPaths = append(searchPaths, args...)
-
-		files, err := util.MaybeResourcesfromPaths(searchPaths)
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
-		basePath, err := os.Getwd()
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
-		cnx := context.Background()
-
-		c, err := console.NewApiClient(cnx, host, apiKeyId, apiKeySecret, org)
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
-		changes, err := release.FindChanges(cnx, c, files)
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
-		release.LockChanged(changes, managedFrom)
-
-		err = validation.Validate(cnx, c, files, searchPaths, basePath, ghOut, false, changes.IdToFileName, concurrentReq)
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
-		err = release.Sync(cnx, c, changes, dryRun, false)
-		if err != nil {
-			snplog.LogFatal(err)
-		}
-
+		runDpWorkflow(cmd, args, func(cnx context.Context, c *console.ApiClient, changes *release.DataProductChangeSet, dryRun bool) error {
+			return release.Sync(cnx, c, changes, dryRun, false)
+		})
 	},
 }
 
 func init() {
 	DataProductsCmd.AddCommand(syncCommand)
-	syncCommand.PersistentFlags().Bool("gh-annotate", false, "Output suitable for github workflow annotation (ignores -s)")
-	syncCommand.PersistentFlags().BoolP("dry-run", "d", false, "Only print planned changes without performing them")
-	syncCommand.PersistentFlags().IntP("concurrency", "c", 3, "The number of validation requests to perform at once (maximum 10)")
+	addCommonDpFlags(syncCommand)
 }
